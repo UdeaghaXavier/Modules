fa5ed3b28928a28ec76e0cbb5e4425b1a9c7cecd
UdeaghaXavier <142506314+UdeaghaXavier@users.noreply.github.com>
2023-09-02 12:08:46 +0100

    first commit

---
+++ highscore.py
@@ -0,0 +1,34 @@
+class highscore:
+	def __init__(self, file, score):
+		""" __init__
+
+		Args:
+			file ([string]): [directory to the txt file] \n
+			score ([string]): [score]
+		"""
+  
+		self.file = file
+		self.score = score
+ 
+	def read_file(self):
+		with open(self.file, 'r') as File:
+			data = File.read()
+		return data
+
+	def write_to_file(self, data = ''):
+		with open(self.file, 'w') as File:
+			File.write(data)
+
+	def clear_file(self):
+		self.write_to_file()
+
+	def main(self):
+		# Only stores the score in the given txt file if the score given is greater
+		# than the value currently in the txt file, if it is empty store the value 
+		data = self.read_file()
+		if data:
+			if self.n > int(data):
+				self.write_to_file(str(self.score))
+			return
+		self.write_to_file(str(self.score))
+		
